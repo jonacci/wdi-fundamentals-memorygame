4a05cf1ace90054766504a98911af32dd2cefff6
jonacci <johanna@changehouse.io>
2017-07-21 08:47:00 -0400

    Fundamentals game
    fist to five.. 3 because I was able to learn html and css on my own. I
    did not do well with the javascript. I need to practice all the basics
    one by one for more than a day for it to stick. I will be learning
    javascript over the weekend at my own pace. I am able to read some js
    code, but I am not yet confidant nor comfortable with writing the code
    for the instructions given in fundamentals.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardTwo);
+//what type of loop and I creating?
+//the array should be a string [queen, king, etc]
+
+//'use strict'
+// Object.create({}) or new Object()
+
+//const television = {
+//  isOn: false,
+  //volume: 10,
+  //channel: 2,
+//  type: 'plasma',
+  //resolution: '1060 x 480',
+  //warranty: true,
+  //brand: 'RCA',
+  //weight: '70 lbs',
+  //maxVolume: 11,
+  //sources: ['HMDI', 'VGA'],
+//  togglePower: function () {
+  //  television.isOn = !television.isOn
+  //},
+  /*increaseVolume: function () {
+    if (television.volume < 11) {
+      television.volume = television.volume + 1
+      if (television.volume === 11) {
+        return 'Maxxed'
+      } else {
+        return 'Volume set to ' + television.volume
+      }
+    } else {
+      return 'Maxxed'
+    }
+  },
+  decreaseVolume: function () {
+    if (television.volume > 0) {
+      television.volume = television.volume - 1
+      return 'Volume set to ' + television.volume
+    } else {
+      return 'Muted'
+    }
+  }
+} */
+
+//////////////////////
+// Variables/Arrays //
+//////////////////////
+
+ alert("Prevent this page from creating additional dialogs.");
+
+var card = { ["queen", "queen", "king", "king"]
+
+}
+
+var cardsInPlay = [];
+
+console.log()
